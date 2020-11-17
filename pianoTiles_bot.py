b8a026efb3cc581bed9dd864fc30b838608ee8e4
danivngopro <44786079+danivngopro@users.noreply.github.com>
2020-11-17 14:34:33 +0200

    working now on the new piano tiles 2

--- pianoTiles_bot.py
+++ pianoTiles_bot.py
@@ -4,11 +4,20 @@ from mss import mss
 import keyboard
 
 start_x = 983
-start_y = 474
+start_y = 440
 
 cords_x = [0, 90, 177, 251]
 
 bbox = (start_x,start_y,start_x+252,start_y + 4)
+
+def test_time():
+    with mss() as sct:
+        t1 = time.time()
+        for i in range(100):
+            img = sct.grab(bbox)
+            pg.click(86,100)
+        t2 = time.time()
+        print(t2-t1)
 
 def get_mouse_pos():
     while True:
@@ -20,23 +29,40 @@ def get_pixel_color():
         img = sct.grab(bbox)
         for cord in cords_x:
             print(img.pixel(cord,0))
-            break
 
 def start():
     with mss() as sct:
+        b = True
+        c = cords_x[0]
         while True:
             if keyboard.is_pressed('q'):
                 break
             img = sct.grab(bbox)
-            for cord in cords_x:
-                if img.pixel(cord,0)[0] < 100:
-                    pg.moveTo(start_x+cord,start_y)
-                    pg.mouseDown(button='left')
-                    if img.pixel(cord,2)[0] > 154:
-                        pg.mouseUp(button='left')
+            if(b == False):
+                    print("x,y= " + str(img.pixel(c,2)) + " pixel: " + str(img.pixel(c,2)[0]))
+                    if img.pixel(c,2)[0] > 154:
+                        pg.mouseUp()
+                        print("finish click")
+                        b = True
+            else:
+                for cord in cords_x:
+                    if img.pixel(cord,0)[0] < 100:
+                        pg.moveTo(start_x+cord,start_y)
+                        pg.mouseDown()
+                        print("start click")
+                        b = False
+                        c = cord
                         break
+                    # while(True):
+                    #     print("x,y= " + str(img.pixel(cord,2)) + " pixel: " + str(img.pixel(cord,2)[0]))
+                    #     if img.pixel(cord,2)[0] > 154:
+                    #         img = sct.grab(bbox)
+                    #         pg.mouseUp()
+                    #         print("finish click")
+                    #         break
+                              
 
-time.sleep(3)
+time.sleep(1)
 start()
 #984 474
 #1074 479
